16c258c93925242e9d711b963ede041bf7fb281f
dmytro <dmytro.malenyk@voco.ee>
2025-11-24 15:37:33 +0200

    kirjalduse lühilause

---
+++ praks1/git_kasutamine.sh
@@ -0,0 +1,20 @@
+
+*root kasutajana:
+install - apt-get install git
+* tavakasutajana:
+esmakordselt:
+
+a) seadista - määra kasutajanimi, email, editor
+git config --global user.name "Ess Perenimi"
+git config --global user.email kasutaja@nimi
+git config --global core.editor nano
+
+b) loo kasaloog, kus hakkad skriptima
+mkdir skriptimine
+
+c) sisene antud kausta
+cd skriptimine
+
+d) tekida lokaalne repositiipium git süsteemi jaols antud kaust:
+git init
+
